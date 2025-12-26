e1b486459f1ebd561fa552679feecff016f6b958
Kormákur <korri123@gmail.com>
2025-12-26 01:36:12 +0000

    fix undefined behavior

--- src/game/combatai.cc
+++ src/game/combatai.cc
@@ -533,7 +533,7 @@ static int ai_find_attackers(Object* critter, Object** a2, Object** a3, Object**
         *a3 = NULL;
     }
 
-    if (*a4 != NULL) {
+    if (a4 != NULL) {
         *a4 = NULL;
     }
 
